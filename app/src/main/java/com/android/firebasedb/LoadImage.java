2c3ab1c621d38f7cac91ff377b7b1288a582e30b
ASUS <anekekarina99@gmail.com>
2020-12-23 22:48:32 +0700

    first

---
+++ app/src/main/java/com/android/firebasedb/LoadImage.java
@@ -0,0 +1,49 @@
+package com.android.firebasedb;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+class LoadImage extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private  DatabaseReference first = databaseReference.child("file_gambar");
+    ImageView img;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_image);
+        img = findViewById(R.id.img);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        first.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String link = dataSnapshot.getValue(String.class);
+                Picasso.get().load(link).into(img);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
